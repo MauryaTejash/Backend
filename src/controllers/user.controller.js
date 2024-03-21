765b2caffe4c6680691d2a05c411d20cef5d04fc
Tejash Maurya <mauryatejash00@gmail.com>
2024-03-21 23:38:00 +0530

    access and refresh token generation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -209,7 +209,7 @@ const loggedOut = asyncHandler(async(req,res)=>{
 const refereshAccessToken = asyncHandler(async(req,res)=>{
     const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401,"Unauthorized access")
     }
     //verification of user token and DB token
